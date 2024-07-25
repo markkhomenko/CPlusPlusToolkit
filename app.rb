96a595736ee85ea808ca49f49808aa4b4d77f3e1
markkhomenko <156857659+markkhomenko@users.noreply.github.com>
2024-07-25 16:28:59 +0300

    initial_commit

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'sinatra/base'
+require './lib/player'
+require './lib/game'
+
+
+class Battle < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    player_1 = Player.new(params[:name1])
+    player_2 = Player.new(params[:name2])
+    @game = Game.create(player_1, player_2)
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @game = Game.instance
+    @game.turn_switcher
+    erb :play
+  end
+
+  get '/attack' do
+    @game = Game.instance
+    @game.attack
+    erb :attack
+  end
+
+  run! if app_file == $0
+end
